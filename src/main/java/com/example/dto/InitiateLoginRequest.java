4f6b63a7dfb6b7104cdd8a0051222c6525244270
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 17:37:20 +0530

    Add  src/main/java/com/example/dto/InitiateLoginRequest.java

---
+++ src/main/java/com/example/dto/InitiateLoginRequest.java
@@ -0,0 +1,19 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InitiateLoginRequest {
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+[1-9]\\d{1,14}$", message = "Phone number must be in E.164 format")
+    private String phoneNumber;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
