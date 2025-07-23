1e89b36f0a08c89d224c6cee9dc1504ad01c6ae5
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 15:52:13 +0530

    Add  src/main/java/com/example/exception/ApiException.java

---
+++ src/main/java/com/example/exception/ApiException.java
@@ -0,0 +1,14 @@
+package com.example.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public abstract class ApiException extends RuntimeException {
+    private final HttpStatus status;
+
+    protected ApiException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+}
