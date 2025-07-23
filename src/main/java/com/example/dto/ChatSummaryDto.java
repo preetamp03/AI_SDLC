eec2a177d1a64fabdbdfc10894512db30bac277d
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 17:37:19 +0530

    Add  src/main/java/com/example/dto/ChatSummaryDto.java

---
+++ src/main/java/com/example/dto/ChatSummaryDto.java
@@ -0,0 +1,16 @@
+package com.example.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class ChatSummaryDto {
+    private UUID id;
+    private UserSummaryDto recipient;
+    private MessageSummaryDto lastMessage;
+    private long unreadCount;
+    private boolean isRead;
+}
