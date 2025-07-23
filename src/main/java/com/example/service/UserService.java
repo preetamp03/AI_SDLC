95be4dd7ef873d180bc4ade22337fbd3ff54d147
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 15:52:39 +0530

    Add  src/main/java/com/example/service/UserService.java

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import com.example.exception.ResourceNotFoundException;
+import com.example.model.User;
+import com.example.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+
+    private final IUserRepository userRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<User> findByPhoneNumber(String phoneNumber) {
+        return userRepository.findByPhoneNumber(phoneNumber);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User findById(UUID id) {
+        return userRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + id));
+    }
+}
