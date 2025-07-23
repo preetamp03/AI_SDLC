363a286fa079be0d9183faa58752a0f666a41283
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 15:52:10 +0530

    Add  src/main/java/com/example/dto/PaginationDto.java

---
+++ src/main/java/com/example/dto/PaginationDto.java
@@ -0,0 +1,19 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaginationDto {
+    private int currentPage;
+    private int totalPages;
+    private int pageSize;
+    private long totalCount;
+    private boolean hasNext;
+    private boolean hasPrevious;
+}
