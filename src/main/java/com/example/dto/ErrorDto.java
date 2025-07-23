243b2517c9b0f7cb2ee64367cf32f91c5752cf02
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 15:52:12 +0530

    Add  src/main/java/com/example/dto/ErrorDto.java

---
+++ src/main/java/com/example/dto/ErrorDto.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto {
+    private int statusCode;
+    private String message;
+    private String error;
+}
