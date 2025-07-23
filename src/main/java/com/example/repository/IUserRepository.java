3a782867a3374ecf7d7faa4e99ceccceaebf8cda
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-23 15:34:05 +0530

    Add  src/main/java/com/example/repository/IUserRepository.java

---
+++ src/main/java/com/example/repository/IUserRepository.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+import com.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByPhoneNumber(String phoneNumber);
+}
+```
+```java
+// src/main/java/com/example/repository/IChatRepository.java
