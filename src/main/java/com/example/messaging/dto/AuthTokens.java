606762c271e946979780a93701f1babc94afaa08
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-28 16:13:09 +0530

    Add  src/main/java/com/example/messaging/dto/AuthTokens.java

---
+++ src/main/java/com/example/messaging/dto/AuthTokens.java
@@ -0,0 +1,13 @@
+package com.example.messaging.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthTokens {
+    private String accessToken;
+    private String refreshToken;
+}
