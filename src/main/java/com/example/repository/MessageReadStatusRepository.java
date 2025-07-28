e4024312eb520e650fe989f50bbb6c5cff62044d
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-28 14:46:14 +0530

    Delete src directory

--- src/main/java/com/example/repository/MessageReadStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.model.MessageReadStatus;
-import com.example.model.MessageReadStatusId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageReadStatusRepository extends JpaRepository<MessageReadStatus, MessageReadStatusId> {
-}
