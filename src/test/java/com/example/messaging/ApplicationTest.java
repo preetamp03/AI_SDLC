dcac82ee679db32fdd2de07ba763a070efe7ac69
preetamp03 <93396243+preetamp03@users.noreply.github.com>
2025-07-28 16:13:01 +0530

    Add  src/test/java/com/example/messaging/ApplicationTest.java

---
+++ src/test/java/com/example/messaging/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    /**
+     * Test to ensure the Spring application context loads successfully.
+     */
+    @Test
+    void contextLoads() {
+    }
+}
